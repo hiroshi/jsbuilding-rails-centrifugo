82680c02c9e500077c61ccaf0c105bba7678e0d8
hiroshi <hiroshi3110@gmail.com>
2024-05-26 19:16:36 +0900

    skip csrf protection if api request.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   include SessionHelper
 
+  skip_before_action :verify_authenticity_token
+  before_action :verify_authenticity_token, unless: -> { api_request? }
+
   rescue_from AppError do |exception|
     render plain: exception, status: exception.status
   end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -5,10 +5,16 @@ module SessionHelper
 
   def current_user
     @current_user ||= session[:user_id] && User.find(session[:user_id])
-    @current_user ||= User.authorize(token: request.authorization.sub(/bearer\s+/i, ''))
+    @current_user ||= User.authorize(token: request.authorization&.sub(/bearer\s+/i, '')).tap do |user|
+      @api_request = user.present?
+    end
   end
 
   def authenticate_user!
     raise AppError.new('Not authenticated', status: :forbidden) unless current_user
+  end
+
+  def api_request?
+   current_user &&  @api_request
   end
 end
